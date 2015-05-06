309ffaf776e6200aebe57eb15a432eccf6aa8092
shrnchn <schan21@live.com>
2015-05-06 10:11:08 -0400

    fixed linking scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ $(window).load(function(){
 
 
 $(document).ready(function() {
+
+	$('.mobile-nav').click(function() {
+		$('#header-burger').fadeToggle();
+	});	
 	
 	var waypoint = new Waypoint({
 		element: $('#howitworks'),
@@ -18,20 +22,9 @@ $(document).ready(function() {
 			}
 		}
 	});
-	
-	$("#features-click, #features-click2").click(function() {
-	    $('html, body').stop().animate({
-	        scrollTop: $("#howitworks").offset().top
-	    }, 600);
-	});
-	
-	$('.mobile-nav').click(function() {
-		$('#header-burger').fadeToggle();
-	});	
 
 	var $scrollingDiv = $("#faq-nav ul");
 
-	 
 	$(window).scroll(function(){
 		$scrollingAmount = $(window).scrollTop() - 180;
 		
@@ -40,6 +33,12 @@ $(document).ready(function() {
 			$scrollingAmount = 0;
 		}
 		$scrollingDiv.stop().animate({"marginTop": $scrollingAmount + "px"}, 'slow' );
+	});
+
+	$("#features-click, #features-click2").click(function() {
+	    $('html, body').stop().animate({
+	        scrollTop: $("#howitworks").offset().top
+	    }, 600);
 	});
 
 });
